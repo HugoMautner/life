a808e8858b950297ade4b9070ac319b8946af937
Hugo Mautner <56817692+HugoMautner@users.noreply.github.com>
2023-10-16 18:23:58 +0200

    fixed spacing/indents

--- Main.scala
+++ Main.scala
@@ -1,20 +1,20 @@
 package life
 
 object Main:
-  val help = """
-    Welcome to GAME OF LIFE!
+    val help = """
+        Welcome to GAME OF LIFE!
 
-    Click on cell to toggle.
-    Press ENTER for next generation.
-    Press SPACE to toggle play/stop.
-    Press R to create random life.
-    Press BACKSPACE to clear life.
-    Close window to exit.
-  """
+        Click on cell to toggle.
+        Press ENTER for next generation.
+        Press SPACE to toggle play/stop.
+        Press R to create random life.
+        Press BACKSPACE to clear life.
+        Close window to exit.
+    """
 
-  val dim = (30, 40)
+    val dim = (30, 40)
 
-  def main(args: Array[String]): Unit =
-    println(help)
-    //val w = new LifeWindow(dim._1, dim._2)
-    //w.start()
+    def main(args: Array[String]): Unit =
+        println(help)
+        //val w = new LifeWindow(dim._1, dim._2)
+        //w.start()
