d369f00b7c5c90d16b2c74b5748e7f52e614b6b3
HugoMautner <hugo.mautner@gmail.com>
2023-10-27 15:00:28 +0200

    started working

--- Life.scala
+++ Life.scala
@@ -1,20 +1,33 @@
 package life
 
+import math.random
+
 case class Life(cells: Matrix[Boolean]):
 
     /** Ger true om cellen på plats (row, col) är vid liv annars false.
     * Ger false om indexeringen är utanför universums gränser.
     */
-    def apply(row: Int, col: Int): Boolean = ???
+    def apply(row: Int, col: Int): Boolean = 
+        // check if out of universe bounds
+        if row < 0 || row > LifeWindow.windowSize._2 || col < 0 || col > LifeWindow.windowSize._1 then false
+        
+        cells(row)(col)
 
     /** Sätter status på cellen på plats (row, col) till value. */
-    def updated(row: Int, col: Int, value: Boolean): Life = ???
+    def updated(row: Int, col: Int, value: Boolean): Life = 
+        cells(row)(col) = value
 
     /** Växlar status på cellen på plats (row, col). */
-    def toggled(row: Int, col: Int): Life = ???
+    def toggled(row: Int, col: Int): Life = 
+        cells(row)(col) = !cells(row)(col)
 
     /** Räknar antalet levande grannar till cellen i (row, col). */
-    def nbrOfNeighbours(row: Int, col: Int): Int = ???
+    def nbrOfNeighbours(row: Int, col: Int): Int = 
+        var nbrLivingNeighbours: Int = 0
+        for r <- row -1 to row + 1 do 
+            for c <- col -1 to col + 1 do
+                if cells(r)(c) then nbrLivingNeighbours += 1
+        nbrLivingNeighbours - 1
 
     /** Skapar en ny Life-instans med nästa generation av universum.
         * Detta sker genom att applicera funktionen \code{rule} på cellerna.
@@ -22,19 +35,31 @@ case class Life(cells: Matrix[Boolean]):
     def evolved(rule: (Int, Int, Life) => Boolean = Life.defaultRule):Life =
         var nextGeneration = Life.empty(cells.dim)
         cells.foreachIndex( (r,c) =>
-        ???
+        Life.defaultRule(rule)
         )
         nextGeneration
 
     /** Radseparerad text där 0 är levande cell och - är död cell. */
-    override def toString = ???
+    override def toString = 
+        ???
 
 object Life:
     /** Skapar ett universum med döda celler. */
-    def empty(dim: (Int, Int)): Life = ???
+    def empty(dim: (Int, Int)): Life = 
+        Life(Matrix.fill(dim)(false))
 
     /** Skapar ett unviversum med slumpmässigt liv. */
-    def random(dim: (Int, Int)): Life = ???
+    def random(dim: (Int, Int)): Life = 
+        import scala.util.Random.nextBoolean
+        val newLife = Life(Matrix.fill(dim)(false))
+
 
     /** Implementerar reglerna enligt Conways Game of Life. */
-    def defaultRule(row: Int, col: Int, current: Life): Boolean = ???
+    def defaultRule(row: Int, col: Int, current: Life): Boolean = 
+        if current.cells(row)(col) then 
+            if current.nbrOfNeighbours(row, col) > 3 || 
+                current.nbrOfNeighbours(row, col) < 2 then current.toggled(row, col)
+        else 
+            if current.nbrOfNeighbours(row)(col) == 3 then current.toggled(row, col)
+
+        current.cells(row)(col)
--- LifeWindow.scala
+++ LifeWindow.scala
@@ -1,24 +1,37 @@
 package life
 
 import introprog.PixelWindow
-import introprog.PixelWindow.Event
+import java.awt.color as JColor
+import scala.compiletime.ops.int
 
 object LifeWindow:
     val EventMaxWait = 1 // milliseconds
     var NextGenerationDelay = 200 // milliseconds
     // lägg till fler användbara konstanter här tex färger etc.
+    object Colors:
+        val cellLiving = JColor(242,128,161)
+        val space      = JColor(0, 0, 0)
+        val grid       = JColor(255, 255, 255)
+
+    val windowSize = (80, 60)
 
 class LifeWindow(rows: Int, cols: Int):
     import LifeWindow.* // importera namn från kompanjon
 
     var life = Life.empty(rows, cols)
-    val window: PixelWindow = ???
+    val window: PixelWindow = PixelWindow(windowSize)
     var quit = false
     var play = false
 
-    def drawGrid(): Unit = ???
+    def drawGrid(topLeft: Pos) (size: (Int, Int)) (color: JColor): Unit = 
+        for y <- topLeft._2 to topLeft._2 + size._2 do
+            for x <- topLeft._1 to topLeft._1 + size._1 do
+                setBlock(Pos(x, y), color)
 
-    def drawCell(row: Int, col: Int): Unit = ???
+    def drawCell(pos: (Int, Int), color: JColor): Unit = $
+        val x = pos._1 * blockSize
+        val y = pos._2 * blockSize
+        pixelWindow.fill(x, y, blockSize, blockSize, color)
 
     def update(newLife: Life): Unit =
         val oldLife = life
